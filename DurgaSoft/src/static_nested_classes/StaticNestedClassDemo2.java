bfa58ab44792a6e555dfcacbaebb48e2ea254235
manivannan251 <manivannan251@gmail.com>
2019-04-21 09:42:29 +0530

    static class demo

---
+++ DurgaSoft/src/static_nested_classes/StaticNestedClassDemo2.java
@@ -0,0 +1,16 @@
+package static_nested_classes;
+
+public class StaticNestedClassDemo2 {
+	
+	static class Nested {
+		public static void main(String[] args) {
+			System.out.println("Static class main method");
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Main class main method");
+	}
+	
+
+}
