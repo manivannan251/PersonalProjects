26d8a916025b89ec3c29fe318646428506b2734e
manivannan251 <manivannan251@gmail.com>
2019-04-25 07:30:34 +0530

    Refactoring

---
+++ DurgaSoft/src/inner_class/anonymous_inner_class/AnonymousInnerClassDemo.java
@@ -0,0 +1,31 @@
+package inner_class.anonymous_inner_class;
+class Popcorn{
+	public void taste() {
+		System.out.println("salty");
+	}
+}
+public class AnonymousInnerClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Popcorn p = new Popcorn() {
+			public void taste() {
+				System.out.println("spicy");
+			}
+		};
+		p.taste();
+		Popcorn p1 = new Popcorn();
+		p1.taste();
+		Popcorn p2 = new Popcorn() {
+			public void taste() {
+				System.out.println("sweet");
+			}
+		};
+		p2.taste();
+		System.out.println(p.getClass().getName());
+		System.out.println(p1.getClass().getName());
+		System.out.println(p2.getClass().getName());
+
+	}
+
+}
---
+++ DurgaSoft/src/inner_class/static_nested_classes/StaticNestedClassDemo3.java
@@ -0,0 +1,21 @@
+package inner_class.static_nested_classes;
+
+public class StaticNestedClassDemo3 {
+
+	int x=10;
+	static int y=20;
+	static class Nested {
+		public void m1() {
+			System.out.println(y);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Nested n = new Nested();
+		n.m1();
+	}
+
+}
