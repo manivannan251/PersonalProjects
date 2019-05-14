3a875c38a9b048c94e0f679322bef56100e3a3b2
manivannan251 <manivannan251@gmail.com>
2019-05-14 21:17:57 +0530

    code for generics demo

---
+++ DurgaSoft/src/generics/GenericsDemo4.java
@@ -0,0 +1,17 @@
+package generics;
+
+import java.util.ArrayList;
+
+public class GenericsDemo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList l = new ArrayList<String>();
+		l.add(10);
+		l.add(10.5);
+		l.add("Hello");
+		System.out.println(l);
+
+	}
+
+}
